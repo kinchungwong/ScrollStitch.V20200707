538c38276ae1b3c804cb60413fd3a600ead50dbc
kinchungwong <kinchungwong@users.noreply.github.com>
2020-07-22 19:23:26 -0700

    (stub added) Config.ConfigVariableSubstitutions. (Intention) A class for applying variable substitutions to config string fields.

---
+++ ScrollStitch.V20200707/Config/ConfigVariableSubstitutions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScrollStitch.V20200707.Config
+{
+    /// <summary>
+    /// A class for applying variable substitutions to config string fields.
+    /// </summary>
+    public class ConfigVariableSubstitutions
+    {
+    }
+}
