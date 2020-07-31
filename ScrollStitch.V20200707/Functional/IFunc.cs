1dbef8e514a608450b84af91d08c9c86103658cd
kinchungwong <kinchungwong@users.noreply.github.com>
2020-07-30 21:33:16 -0700

    (bugfix, breaking, functional) Fix IFunc commit 739c02a3. Generic type parameters had the wrong order, did not match that of CLR Func delegate.

--- ScrollStitch.V20200707/Functional/IFunc.cs
+++ ScrollStitch.V20200707/Functional/IFunc.cs
@@ -6,37 +6,37 @@ using System.Threading.Tasks;
 
 namespace ScrollStitch.V20200707.Functional
 {
-    public interface IFunc<TResult>
+    public interface IFunc<out TResult>
     {
         TResult Invoke();
     }
 
-    public interface IFunc<TResult, T1>
+    public interface IFunc<in T1, out TResult>
     {
         TResult Invoke(T1 t1);
     }
 
-    public interface IFunc<TResult, T1, T2>
+    public interface IFunc<in T1, in T2, out TResult>
     {
         TResult Invoke(T1 t1, T2 t2);
     }
 
-    public interface IFunc<TResult, T1, T2, T3>
+    public interface IFunc<in T1, in T2, in T3, out TResult>
     {
         TResult Invoke(T1 t1, T2 t2, T3 t3);
     }
 
-    public interface IFunc<TResult, T1, T2, T3, T4>
+    public interface IFunc<in T1, in T2, in T3, in T4, out TResult>
     {
         TResult Invoke(T1 t1, T2 t2, T3 t3, T4 t4);
     }
 
-    public interface IFunc<TResult, T1, T2, T3, T4, T5>
+    public interface IFunc<in T1, in T2, in T3, in T4, in T5, out TResult>
     {
         TResult Invoke(T1 t1, T2 t2, T3 t3, T4 t4, T5 t5);
     }
 
-    public interface IFunc<TResult, T1, T2, T3, T4, T5, T6>
+    public interface IFunc<in T1, in T2, in T3, in T4, in T5, in T6, out TResult>
     {
         TResult Invoke(T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6);
     }
