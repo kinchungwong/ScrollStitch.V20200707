28345bbb906648e74f62e4a1b077dab3dcf3dc04
kinchungwong <kinchungwong@users.noreply.github.com>
2020-08-03 00:38:39 -0700

    (minor, comment) CoreCharArrayInfo added class xml comments.

--- ScrollStitch.V20200707.FixedWidthFontExtractionTool/Imaging/FontExtraction/CoreCharArrayInfo.cs
+++ ScrollStitch.V20200707.FixedWidthFontExtractionTool/Imaging/FontExtraction/CoreCharArrayInfo.cs
@@ -9,6 +9,34 @@ namespace ScrollStitch.V20200707.Imaging.FontExtraction
     using Data;
     using Text;
 
+    /// <summary>
+    /// This class creates a text template that is used to assist in image font extraction.
+    /// 
+    /// <para>
+    /// This class prints out all ASCII printable characters (in the char range 32 to 127)
+    /// in a template with a predefined layout.
+    /// </para>
+    /// 
+    /// <para>
+    /// After rendering the text with a fixed-width ASCII font, a screenshot of the rendered
+    /// text can then be processed with <see cref="FixedWidthFontRectExtractor"/>.
+    /// </para>
+    /// 
+    /// <para>
+    /// Reminder. <br/>
+    /// <see cref="FixedWidthFontRectExtractor"/> requires access to the same text template 
+    /// class that generated the text captured in the screenshot. Usually, the character array
+    /// from <see cref="DuplexCharArrayInfo"/> should be used. 
+    /// </para>
+    /// 
+    /// <para>
+    /// The main difference between <see cref="DuplexCharArrayInfo"/> and <see cref="CoreCharArrayInfo"/> 
+    /// is that alignment marks are added outside the four corners of the text array, and that each 
+    /// character is duplicated in a 2-by-2 cell. These modifications to the character array are needed 
+    /// by the <see cref="FixedWidthFontRectExtractor"/> algorithm.
+    /// </para>
+    /// </summary>
+    /// 
     public sealed class CoreCharArrayInfo 
         : ICharArray
     {
