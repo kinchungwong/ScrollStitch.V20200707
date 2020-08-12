b44857cf73b3e10d2ddca478b0dc811846de45fb
kinchungwong <kinchungwong@users.noreply.github.com>
2020-08-11 21:22:19 -0700

    (spatial) IRectQuery added.

---
+++ ScrollStitch.V20200707/Spatial/IRectQuery.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScrollStitch.V20200707.Spatial
+{
+    using ScrollStitch.V20200707.Data;
+
+    public interface IRectQuery<T>
+    {
+        /// <summary>
+        /// Enumerates all items that have a positive-area overlap with the query rectangle.
+        /// 
+        /// <para>
+        /// Normal functioning is not guaranteed if the collection has been modified after the instantiation 
+        /// of the <see cref="IEnumerable{T}"/>. This is not an implementation requirement.
+        /// </para>
+        /// </summary>
+        /// <param name="queryRect">
+        /// The query rectangle.
+        /// </param>
+        /// <returns>
+        /// An enumeration of items having a positive-area overlap with the query rectangle.
+        /// </returns>
+        IEnumerable<T> Enumerate(Rect queryRect);
+    }
+}
