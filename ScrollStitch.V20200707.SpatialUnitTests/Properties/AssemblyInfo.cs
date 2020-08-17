6c4cf2f3739ee692c6a5bab658daa7eccfff2cc9
kinchungwong <kinchungwong@users.noreply.github.com>
2020-08-17 08:25:09 -0700

    SpatialUnitTests project added
    Initial commit. Currently blank.

---
+++ ScrollStitch.V20200707.SpatialUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ScrollStitch.V20200707.SpatialUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ScrollStitch.V20200707.SpatialUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("038b3936-b9ca-4471-b0e0-6085fe8f25ae")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
